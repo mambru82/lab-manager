2057a84f41c9645a870f4edf3f1ce6388e1ca0e5
MGcommits <marthagamez211@gmail.com>
2021-03-31 12:56:04 -0500

    Changes in signup and login pages

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,7 +7,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector("#password-login").value.trim();
 
   if (username && password) {
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/techs/login", {
       method: "post",
       body: JSON.stringify({
         email,
