116334aed11288bc8078c4c7ca1c2b4b19a2da5b
mambru <ecastro82@gmail.com>
2021-03-31 22:26:19 -0500

    added results-routes, fixed tech to results relationship :

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const runRoutes = require('./run-routes');
 const techRoutes = require('./tech-routes')
 
 router.use('/patient', patientRoutes);
-// router.use('/results', resultsRoutes);
+router.use('/results', resultsRoutes);
 // router.use('/run', runRoutes);
 
 module.exports = router;
