20b03d983a06652d0589e9044995a50b6fc14c08
mambru <ecastro82@gmail.com>
2021-04-06 13:23:23 -0500

    update results function is working

--- public/javascript/submit-results.js
+++ public/javascript/submit-results.js
@@ -1,27 +1,28 @@
 async function resultFormHandler(event) {
     // console.log(event);
-    
-    result_body = document.querySelector("#new_results").value.trim();
+    event.preventDefault()
+  
+    let result_id = parseInt(document.querySelector("#result_id").innerHTML)
+    let result_body = document.getElementById("new_results").value.trim();
+    console.log(result_id);
     console.log(result_body);
-    //  if (result_body) {
-    //    const response = await fetch(`/api/results/${id}`, {
-    //      method: "PUT",
-    //      body: JSON.stringify({
-    //        result_body
-    //      }),
-    //      headers: {
-    //        "Content-Type": "application/json",
-    //      },
-    //    });
+     if (result_body) {
+       const response = await fetch(`/api/results/${result_id}`, {
+         method: "PUT",
+         body: result_body,
+         headers: {
+           "Content-Type": "application/json",
+         },
+       });
 
-    //    if (response.ok) {
-    //      document.location.reload();
-    //    } else {
-    //      alert(response.statusText);
-    //    }
-    //  }
-    //  var notification = encodeURIComponent("Results updated succesfully");
-    //  window.location.replace("/enter-results?notification=" + notification);
+       if (response.ok) {
+         document.location.reload();
+       } else {
+         alert(response.statusText);
+       }
+     }
+     var notification = encodeURIComponent("Results updated succesfully");
+     window.location.replace("/enter-results?notification=" + notification);
 }
 
 document.querySelector(".submit-results-form");
