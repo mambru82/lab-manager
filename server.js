86fc1a905c55f8ba7e22ae49cd14f87cd023540b
T. Curry <74472798+curryduz@users.noreply.github.com>
2021-04-07 18:34:28 -0500

    fixed code

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session(sess));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
